d5f9c9ecc88aa29439e0e34a74a5e2cf6b00bc11
Santiago Hollmann <santiago.hollmann@gmail.com>
2019-08-25 19:01:10 -0300

    Added pull to refresh and made some small refactors

--- app/src/main/java/com/shollmann/topreddit/ui/toplist/ItemListActivity.kt
+++ app/src/main/java/com/shollmann/topreddit/ui/toplist/ItemListActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.shollmann.topreddit.R
+import com.shollmann.topreddit.model.Post
 import kotlinx.android.synthetic.main.activity_item_list.*
 import kotlinx.android.synthetic.main.item_list.*
 import kotlinx.coroutines.Dispatchers
@@ -26,14 +27,11 @@ class ItemListActivity : AppCompatActivity() {
         viewmodel.topPosts.observe(
             this,
             Observer {
-                val adapter = item_list.adapter as PostListAdapter
-                adapter.values = it
-                adapter.notifyDataSetChanged()
+                displayPosts(it)
             })
 
-        GlobalScope.launch(Dispatchers.Main) {
-            viewmodel.loadTopPosts()
-        }
+        swipe_refresh_layout.isRefreshing = true
+        fetchPosts(viewmodel)
 
         if (item_detail_container != null) {
             // If this view is present, then the
@@ -42,5 +40,22 @@ class ItemListActivity : AppCompatActivity() {
         }
 
         item_list.adapter = PostListAdapter(this, emptyList(), twoPane)
+
+        swipe_refresh_layout.setOnRefreshListener {
+            fetchPosts(viewmodel)
+        }
+    }
+
+    private fun displayPosts(it: List<Post>) {
+        swipe_refresh_layout.isRefreshing = false
+        val adapter = item_list.adapter as PostListAdapter
+        adapter.values = it
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun fetchPosts(viewmodel: TopListViewModel) {
+        GlobalScope.launch(Dispatchers.Main) {
+            viewmodel.loadTopPosts()
+        }
     }
 }
